a5fcbd4bcfecde457cab19ef0874c74ffdde00eb
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-27 16:20:50 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,48 +1,28 @@
-// We use class syntax to define our extension object
-// This isn't actually necessary, but it tends to look the best
-
-class MyExtension {
-  /**
-   * Scratch will call this method *once* when the extension loads.
-   * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
-   */
+class piExtension {
   getInfo() {
     return {
       // `id` is the internal ID of the extension
-      // It should never change!
-      // If you choose to make an actual extension, please change this to something else.
       // Only the characters a-z and 0-9 can be used. No spaces or special characters.
-      id: 'pi_extension',
+      id: 'piextension',
 
       // `name` is what the user sees in the toolbox
-      // It can be changed without breaking projects.
       name: 'π Extension',
 
-      blocks: [
-        {
+      blocks: [ {
           // `opcode` is the internal ID of the block
           // It should never change!
           // It corresponds to the class method with the same name.
           opcode: 'pi',
           blockType: Scratch.BlockType.REPORTER,
           text: 'π (pi)'
-        }
-      ]
+        } ]
       
     };
   }
 
-  /**
-   * Corresponds to `opcode: 'pi'` above
-   */
-  pi() {
-    // You can just return a value: any string, boolean, or number will work.
-    // If you have to perform an asynchronous action like a request, just return a Promise.
-    // The block will wait until the Promise resolves and return the resolved value.
-    return '3.14159265359';
-  }
+  pi() {return '3.14159265359';} // * Corresponds to `opcode: 'pi'` above
 }
 
 // Call Scratch.extensions.register to register your extension
 // Make sure to register each extension exactly once
-Scratch.extensions.register(new MyExtension());
+Scratch.extensions.register(new PiExtension());
